7e766c32060641b6cc8c14888b5a62959f905f58
Filip Krstevski <83398437+filip77487@users.noreply.github.com>
2021-06-01 11:19:22 +0200

    UpperCase and Reverse Array

---
+++ Homework1.java
@@ -0,0 +1,32 @@
+package dayEight;
+
+public class Homework1 {
+
+	public static void main(String[] args) {
+		String s1 = "123456789";
+		
+		
+//		for (int i = 0; i < s1.length(); i++) {
+//			System.out.print(s1.charAt(i) + ", ");
+			
+		
+			
+			for(int i1 = s1.length()-1; i1 >= 0; i1--) {
+				System.out.print(s1.charAt(i1) + ", ");
+			}
+			
+			
+			System.out.println();
+			
+		String s2 = "i really love this city!!!";
+		System.out.println(s2.toUpperCase());
+		
+		
+		
+		
+}
+	}
+
+
+
+
